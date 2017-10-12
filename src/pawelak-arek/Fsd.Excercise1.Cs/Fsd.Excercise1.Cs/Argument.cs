1b641a7547bd6a7f505605f3d95e9bedf2e317a0
Arek-PC\Arek <arek0601@gmail.com>
2017-10-12 21:14:14 +0200

    completed task

--- src/pawelak-arek/Fsd.Excercise1.Cs/Fsd.Excercise1.Cs/Argument.cs
+++ src/pawelak-arek/Fsd.Excercise1.Cs/Fsd.Excercise1.Cs/Argument.cs
@@ -5,46 +5,71 @@ namespace Fsd.Excercise1.Cs
 {
     class Argument
     {
-
         public void ArgumentInfo(string[] arg)
         {
-            foreach(string str in arg)
+            if (arg.Length != 0)
             {
-                Console.WriteLine(GetSumUpperLetters(str));
+                Console.WriteLine("Console arguments: \n");
+                for (int i = 0; i < arg.Length; i++)
+                {
+                    Console.WriteLine($"Argument {i + 1} of {arg.Length}");
+                    Console.WriteLine($"Value: {arg[i]}");
+                    Console.WriteLine($"Upper letters: {GetSumUpperLetters(arg[i])}");
+                    Console.WriteLine($"Lower letters: {GetSumLowerLetters(arg[i])}");
+                    Console.WriteLine($"Digits: {GetSumDigits(arg[i])} \n");
+                }
             }
+            else
+                Console.WriteLine("Number of arguments = 0");
+        }
+
+        int AnalizeArgument(string argument, ArgumentType argumentType)
+        {
+            int sum=0;
+
+            switch (argumentType)
+            {
+                case ArgumentType.Number:
+                    for (int i = 0; i < argument.Length; i++)
+                    {
+                        if (char.IsNumber(argument[i]))
+                            sum++;
+                    }
+                    break;
+                case ArgumentType.UpperLet:
+                    for (int i = 0; i < argument.Length; i++)
+                    {
+                        if (char.IsUpper(argument[i]))
+                            sum++;
+                    }
+                    break;
+                case ArgumentType.LowerLet:
+                    for (int i = 0; i < argument.Length; i++)
+                    {
+                        if (char.IsLower(argument[i]))
+                            sum++;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return sum;
         }
 
         int GetSumUpperLetters(string argument)
         {
-            int sumUpper=0;
-            for(int i =0;i<argument.Length;i++)
-            {
-                if (char.IsUpper(argument[i]))
-                    sumUpper++;
-            }
-            return sumUpper;
+            return AnalizeArgument(argument, ArgumentType.UpperLet);
         }
 
         int GetSumLowerLetters(string argument)
         {
-            int sumLower = 0;
-            for (int i = 0; i < argument.Length; i++)
-            {
-                if (char.IsLower(argument[i]))
-                    sumLower++;
-            }
-            return sumLower;
+            return AnalizeArgument(argument, ArgumentType.LowerLet);
         }
 
         int GetSumDigits(string argument)
         {
-            int sumDigit = 0;
-            for (int i = 0; i < argument.Length; i++)
-            {
-                if (char.IsLower(argument[i]))
-                    sumDigit++;
-            }
-            return sumDigit;
+            return AnalizeArgument(argument, ArgumentType.Number);
         }
 
     }
--- src/pawelak-arek/Fsd.Excercise1.Cs/Fsd.Excercise1.Cs/Program.cs
+++ src/pawelak-arek/Fsd.Excercise1.Cs/Fsd.Excercise1.Cs/Program.cs
@@ -11,12 +11,21 @@ namespace Fsd.Excercise1.Cs
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Excersice 1 - Introdaction");
+            SetUnderLine(30,'-');
+            Console.WriteLine($"Today is {DateTime.Now}\n");
             Argument arg1 = new Argument();
             arg1.ArgumentInfo(args);
             Console.ReadKey(); 
         }
-
         
+        static void SetUnderLine(int numOfChar, char character)
+        {
+            for(int i =0; i<numOfChar; i++)
+            {
+                Console.Write(character);
+            }
+            Console.WriteLine("");
+        }
     }
 }
