bc900d870f2513e31e2fbcafc0a15077488e714f
Bartosz <bggrafik66@gmail.com>
2017-10-14 14:56:44 +0200

    Start solution

---
+++ src/gawronski-bartosz/Fsd.Gawronski.Bartek.Cs.Ex0/Fsd.Gawronski.Bartek.Cs.Ex0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fsd.Gawronski.Bartek.Cs.Ex0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
