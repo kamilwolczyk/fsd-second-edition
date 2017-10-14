1072e7e0c53a38e288f5adec6aef140d31ee0e96
GoToWinThat <artur.mazela@gmail.com>
2017-10-14 15:55:53 +0200

    Done homework task

---
+++ src/mazela-artur/Fsd.Artur.Cs1/Fsd.Artur.Cs1/Analysis.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fsd.Artur.Cs1
+{
+    public class Analysis
+    {
+        private static int Check(string parameter, int instruction)
+        {
+            int counter = 0;
+
+            if(instruction == 1)
+            {
+                for (var i = 0; i < parameter.Length; i++)
+                {
+                    char item = parameter[i];
+                    if(Char.IsUpper(item) == true)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            else if(instruction == 2)
+            {
+                for (var i = 0; i < parameter.Length; i++)
+                {
+                    char item = parameter[i];
+                    if (Char.IsLower(item) == true)
+                    {
+                        counter++;
+                    }
+                }               
+            }
+            else
+            {
+                for (var i = 0; i < parameter.Length; i++)
+                {
+                    char item = parameter[i];
+                    if (Char.IsDigit(item) == true)
+                    {
+                        counter++;
+                    }
+                }            
+            }
+            
+            return counter;
+        }  
+
+        public static int UpperCounter(string parameter)
+        {
+            return Check(parameter, 1);
+        }
+
+        public static int LowerCounter(string parameter)
+        {
+            return Check(parameter, 2);
+        }
+
+        public static int DigitCounter(string parameter)
+        {
+            return Check(parameter, 3);
+        }
+    }
+}
---
+++ src/mazela-artur/Fsd.Artur.Cs1/Fsd.Artur.Cs1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fsd.Artur.Cs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Writing();
+            Checking(args);
+            Console.ReadKey();
+        }
+        static void Writing()
+        {
+            string marks = "--------------------------------------";
+            Console.WriteLine($"Exercise 1 - Introduction {Environment.NewLine}{marks}{Environment.NewLine}{DateTime.Now.ToShortDateString()}{Environment.NewLine}Console arguments:{Environment.NewLine}");
+ 
+        }
+        static void Checking(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Program have 0 arguments.");
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                    InformationMessage(args[i], i, args.Length);
+            }
+        }
+        static void InformationMessage(string argument_value, int argument_index, int arguments_count)
+        {
+            {
+                Console.WriteLine($"Argument {argument_index} of {arguments_count}{Environment.NewLine}Value: {argument_value}");
+                Console.WriteLine($"Upper letters: { Analysis.UpperCounter(argument_value)}{Environment.NewLine}Lower letters: { Analysis.LowerCounter(argument_value)}{Environment.NewLine}Digits: { Analysis.DigitCounter(argument_value)}");  
+            }
+        }
+    }
+
+}
