11cac757f837bb2d703095223f21c3f8befffcc9
Kamil <kamil.wolczyk@gmail.com>
2017-10-22 15:44:23 +0200

    Library app

---
+++ src/wolczyk-kamil/Fsd.Kamil.InTheClassroom/Weekend2/Weekend2/Domain/Library/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace Weekend2.Domain.Library
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public int PageCount { get; set; }
+
+        public BookType Type { get; set; }
+
+        public Book(string title, int pageCount, BookType type)
+        {
+            Title = title;
+            PageCount = pageCount;
+            Type = type;
+        }
+    }
+}
