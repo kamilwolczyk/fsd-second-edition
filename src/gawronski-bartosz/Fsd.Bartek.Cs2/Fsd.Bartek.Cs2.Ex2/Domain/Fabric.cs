db875317f40f626b7e11a2ec76ca649e5fdb3b8e
Bartosz <bggrafik66@gmail.com>
2017-10-24 15:14:39 +0200

    fabric method implements

--- src/gawronski-bartosz/Fsd.Bartek.Cs2/Fsd.Bartek.Cs2.Ex2/Domain/Fabric.cs
+++ src/gawronski-bartosz/Fsd.Bartek.Cs2/Fsd.Bartek.Cs2.Ex2/Domain/Fabric.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fsd.Bartek.Cs2.Ex2.Domain.Animals;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,52 @@ namespace Fsd.Bartek.Cs2.Ex2.Domain
 {
     class Fabric
     {
+        public Cat[] Cat;
+
+        public Dog[] Dog;
+
+        public Pig[] Pig;
+
+        public Cow[] Cow;
+
+        public void FabricCat(int count)
+        {
+            Cat = new Cat[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Cat[i] = new Animals.Cat(""); //interface method to ask name
+            }
+        }
+
+        public void FabricDog(int count)
+        {
+            Dog = new Dog[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Dog[i] = new Animals.Dog(""); //interface method to ask name
+            }
+        }
+
+        public void FabricPig(int count)
+        {
+            Pig = new Pig[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Pig[i] = new Animals.Pig(""); //interface method to ask name
+            }
+        }
+
+        public void FabricCow(int count)
+        {
+            Cow = new Cow[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Cow[i] = new Animals.Cow(""); //interface method to ask name
+            }
+        }
     }
 }
