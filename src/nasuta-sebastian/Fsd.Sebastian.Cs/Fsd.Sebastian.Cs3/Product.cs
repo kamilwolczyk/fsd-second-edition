87f607a030c308be6f876ac2be7789773efac40e
Sebastian <sebastian.nasuta@gmail.com>
2017-10-28 21:06:02 +0200

    Cs2 & Cs3 - homework

---
+++ src/nasuta-sebastian/Fsd.Sebastian.Cs/Fsd.Sebastian.Cs3/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Fsd.Sebastian.Cs3.Content
+{
+    public class Product
+    {
+        public string Name { get; }
+
+        public int Price { get; }
+
+        public int Count { get; set; }
+
+        public Product(string producer, string model, int price, int count)
+        {
+            Name = $"{producer} {model}";
+            Price = price;
+            Count = count;
+        }
+    }
+}
---
+++ src/nasuta-sebastian/Fsd.Sebastian.Cs/Fsd.Sebastian.Cs3/WarehouseConsole/AddingConsole.cs
@@ -0,0 +1,18 @@
+﻿using Fsd.Sebastian.Cs3.Content;
+using System.Collections.Generic;
+
+namespace Fsd.Sebastian.Cs3.WarehouseConsole
+{
+    public class AddingConsole
+    {
+        public static List<Product> Add(List<Product> products)
+        {
+            products.Add(new Product(ConsolePresenter.GetString("Producer: "), ConsolePresenter.GetString("Model: "), ConsolePresenter.GetInt("Price: "), ConsolePresenter.GetInt("Count: ")));
+
+            ConsolePresenter.ClearConsole();
+
+            return products;
+        }
+    }
+}
+
