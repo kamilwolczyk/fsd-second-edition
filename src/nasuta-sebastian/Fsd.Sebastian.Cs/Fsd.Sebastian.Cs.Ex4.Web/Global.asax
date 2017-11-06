49b3ed115d8e8c66a7699c02cc3c7a9be1ba36dc
Sebastian <sebastian.nasuta@gmail.com>
2017-11-06 18:04:18 +0100

    Ex4 - new project

---
+++ src/nasuta-sebastian/Fsd.Sebastian.Cs/Fsd.Sebastian.Cs.Ex4.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fsd.Sebastian.Cs.Ex4.Web.MvcApplication" Language="C#" %>
---
+++ src/nasuta-sebastian/Fsd.Sebastian.Cs/Fsd.Sebastian.Cs.Ex4.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Fsd.Sebastian.Cs.Ex4.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fsd.Sebastian.Cs.Ex4.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72d3be7b-2902-44aa-a623-f85fae92decf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
