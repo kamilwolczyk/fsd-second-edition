f80058ec585bda163f44dc1ff111eea1e916e52a
zciw <zciw@me.com>
2017-11-17 07:29:32 +0100

    Add files via upload

---
+++ BaseView.cs
@@ -0,0 +1,9 @@
+﻿namespace Fsd.Jacek.Cs2.MVC
+{
+    abstract class BaseView
+    {
+        public BaseView(BaseController controller, BaseModel model)
+        {}
+        public abstract void Update();
+    }
+}
