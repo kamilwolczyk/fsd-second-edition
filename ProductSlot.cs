eac215a5f138f7a7458f36756f124295dbd126f3
zciw <zciw@me.com>
2017-11-17 07:25:22 +0100

    Add files via upload

---
+++ ProductSlot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fsd.Jacek.Cs3.Tools
+{
+   public class ProductSlot
+    {
+        public Product Product { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Fsd.Jacek.Cs3.Tools;
+
+namespace Fsd.Jacek.Cs3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerService customerService = new CustomerService();
+            customerService.Service();
+        }
+    }
+}
