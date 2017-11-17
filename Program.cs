72bf1610da9ef444415cdeafcead05ccfccc1756
zciw <zciw@me.com>
2017-11-17 07:16:56 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,65 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Fsd.Jacek.Cs1.Tools;
 
-namespace fsd_bielewicz_jacek_HomeWork1
+namespace Fsd.Jacek.Cs1
 {
-
-
-
-
-    class Program : Tools
-
+    class Program
     {
-
         static void Main(string[] args)
-
         {
-            DateTime data = DateTime.Today;
-            String dataToPrint = data.ToShortDateString();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Excercise 1 - Inroducktion");
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("Today is " + dataToPrint);
-            Console.WriteLine();
-
-                int countUpper = 0, countLower = 0, countDigit = 0, j;             
-                Int32 argumentsLength = args.Length; 
-                Int32 r = 0; // the varaiable "r" changes number of index of args Array at each iteration of the  for loop below.
-
-                for (int i = 0; i < argumentsLength; i++)
+            View.Begining();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("no arguments");
+            }
+            else
+            {
+                Console.WriteLine("Console arguments:");
+                for (int i = 0; i < args.Length; i++)
                 {
-                    Console.WriteLine("Console Arguments:");
-
-                    if (args.Length > 0)
-                    {
-                        string sentence = args[r];
-                        for (j = 0; j < sentence.Length; j++)
-                        {
-                            if (char.IsUpper(sentence[j])) countUpper++;
-                            if (char.IsLower(sentence[j])) countLower++;
-                            if (char.IsDigit(sentence[j])) countDigit++;
-                        }
-
-                        Console.WriteLine("Argument " + r + " of " + args.Length);
-                        Console.WriteLine("Argument Value: " + args[r]); 
-                        Console.WriteLine("Upper letters: " + countUpper);
-                        Console.WriteLine("Lower letters: " + countLower);
-                        Console.WriteLine("Digits: " + countDigit);
-                        Console.WriteLine();
-                    }
-
-                    r++;
-                    countUpper = 0;
-                    countLower = 0;
-                    countDigit = 0;
-                    Pause();
+                    int numer = i + 1;
+                    View.ShowingNumberofCharacters(args[i], numer, args.Length);
+                    View.Pause();
                 }
+            }
         }
-
     }
 }
-
---
+++ View.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fsd.Jacek.Cs1.Tools
+{
+    class View
+    {
+        public static void ShowingNumberofCharacters(string argumentValue, int argument_index, int argument_count)
+        {
+            Console.WriteLine($"Argument: {argument_index} of {argument_count}\nArgument Value: {argumentValue}");
+            Console.WriteLine($"Number of Digit is   {Counting.FindNumberOfDigits(argumentValue)}");
+            Console.WriteLine($"\nNumber of Lower is {Counting.FindNumberOfLower(argumentValue)}");
+            Console.WriteLine($"\nNumber of Upper is {Counting.FindNumberOfUpper(argumentValue)}\n");
+        }
+
+        public static void Pause()
+        {
+            Console.WriteLine("Press any key to continue...\n");
+            Console.ReadKey(true);
+        }
+
+        public static void Begining()
+        {
+            Console.WriteLine($"\nExcercise 1 - Inroducktion\n-----------------------------------------------\n");
+            Console.WriteLine(DateTime.Today.ToString("D"));
+            Console.WriteLine("");
+        }
+    }
+}
