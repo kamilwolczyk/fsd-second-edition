e4be55d7c29e49203799025e2f642a50b146f8a4
Sebastian <sebastian.nasuta@gmail.com>
2017-11-21 23:06:46 +0100

    adverts - partial view

---
+++ src/nasuta-sebastian/Fsd.Sebastian.Cs/Fsd.Sebastian.Cs.Ex4.Web/Models/Adverts/AdvertListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fsd.Sebastian.Cs.Ex4.Web.Models.Adverts
+{
+    public class AdvertListModel
+    {
+        public IEnumerable<string> Adverts { get; set; }
+    }
+}
