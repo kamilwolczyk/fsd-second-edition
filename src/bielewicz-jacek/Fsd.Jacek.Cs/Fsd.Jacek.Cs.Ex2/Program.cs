700481d2829d465486858ff37b0d43d4498b0136
zciw <zciw@me.com>
2017-12-01 12:50:14 +0100

    HomeworkEx2

---
+++ src/bielewicz-jacek/Fsd.Jacek.Cs/Fsd.Jacek.Cs.Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Fsd.Jacek.Cs.Ex2.MVC;
+
+namespace Fsd.Jacek.Cs.Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalFactoryController animalFactoryController = new AnimalFactoryController();
+            animalFactoryController.ShowView();
+        }
+    }
+}
