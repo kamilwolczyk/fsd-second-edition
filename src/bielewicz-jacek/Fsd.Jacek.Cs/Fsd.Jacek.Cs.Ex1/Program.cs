c663dae83fc8a03b32b684c228fd51955491236e
zciw <zciw@me.com>
2017-12-01 12:45:49 +0100

    Homework

---
+++ src/bielewicz-jacek/Fsd.Jacek.Cs/Fsd.Jacek.Cs.Ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Fsd.Jacek.Cs.Ex1.Tools;
+
+namespace Fsd.Jacek.Cs.Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            View.Begining();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("no arguments");
+            }
+            else
+            {
+                Console.WriteLine("Console arguments:");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    int numer = i + 1;
+                    View.ShowingNumberofCharacters(args[i], numer, args.Length);
+                    View.Pause();
+                }
+            }
+
+        }
+    }
+}
---
+++ src/bielewicz-jacek/Fsd.Jacek.Cs/Fsd.Jacek.Cs.Ex1/Tools/Counting.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fsd.Jacek.Cs.Ex1.Tools
+{
+    class Counting
+    {
+        public static int FindNumberOfDigits(string phraseStr)
+        {
+            int countDigit = 0;
+            if (countDigit < phraseStr.Length)
+            {
+                for (var i = 0; i < phraseStr.Length; i++)
+                {
+                    char phraseChar = phraseStr[i];
+                    if (char.IsDigit(phraseChar) == true)
+                        countDigit++;
+                }
+            }
+            return countDigit++;
+        }
+
+        public static int FindNumberOfUpper(string phraseStr)
+        {
+            int countUpper = 0;
+            if (countUpper < phraseStr.Length)
+            {
+                for (var i = 0; i < phraseStr.Length; i++)
+                {
+                    char phraseChar = phraseStr[i];
+                    if (char.IsUpper(phraseChar) == true)
+                        countUpper++;
+                }
+            }
+            return countUpper++;
+        }
+
+        public static int FindNumberOfLower(string phraseStr)
+        {
+            int countLower = 0;
+            if (countLower < phraseStr.Length)
+            {
+                for (var i = 0; i < phraseStr.Length; i++)
+                {
+                    char phraseChar = phraseStr[i];
+                    if (char.IsLower(phraseChar) == true)
+                        countLower++;
+                }
+            }
+            return countLower++;
+        }
+    }
+}
