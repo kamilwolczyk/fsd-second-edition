b08262479ccde54854266202ec0950de7e7e3192
Kamil <kamil.wolczyk@gmail.com>
2017-12-02 18:19:54 +0100

    Weekend 5 day 1

---
+++ src/wolczyk-kamil/Fsd.Kamil.InTheClassroom/Weekend4/Pzpn.Team/Pzpn.Team.Web/Controllers/Api/PlayersController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Pzpn.Team.Web.Controllers.Api
+{
+    public class PlayersController : ApiController
+    {
+        int x()
+        {
+            return 5;
+        }
+    }
+}
--- src/wolczyk-kamil/Fsd.Kamil.InTheClassroom/Weekend4/Pzpn.Team/Pzpn.Team.Web/Global.asax.cs
+++ src/wolczyk-kamil/Fsd.Kamil.InTheClassroom/Weekend4/Pzpn.Team/Pzpn.Team.Web/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using Autofac;
-using Autofac.Integration.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +9,8 @@ namespace Pzpn.Team.Web
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
