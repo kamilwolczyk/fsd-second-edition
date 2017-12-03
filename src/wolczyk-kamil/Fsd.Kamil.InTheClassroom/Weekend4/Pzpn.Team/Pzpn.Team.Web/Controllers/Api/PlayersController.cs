31e19183050c4de34596cf06676796db75553e0f
Kamil <kamil.wolczyk@gmail.com>
2017-12-03 09:01:35 +0100

    API

--- src/wolczyk-kamil/Fsd.Kamil.InTheClassroom/Weekend4/Pzpn.Team/Pzpn.Team.Web/Controllers/Api/PlayersController.cs
+++ src/wolczyk-kamil/Fsd.Kamil.InTheClassroom/Weekend4/Pzpn.Team/Pzpn.Team.Web/Controllers/Api/PlayersController.cs
@@ -1,12 +1,23 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http;
+using System.Web.Http;
 
 namespace Pzpn.Team.Web.Controllers.Api
 {
+    [RoutePrefix("api/players")]
     public class PlayersController : ApiController
     {
-        int x()
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Get()
         {
-            return 5;
+            return Ok("5");
+        }
+
+        [HttpPut]
+        [Route("")]
+        public IHttpActionResult DoSthWithPlayer()
+        {
+            return Ok();
         }
     }
 }
--- src/wolczyk-kamil/Fsd.Kamil.InTheClassroom/Weekend4/Pzpn.Team/Pzpn.Team.Web/Global.asax.cs
+++ src/wolczyk-kamil/Fsd.Kamil.InTheClassroom/Weekend4/Pzpn.Team/Pzpn.Team.Web/Global.asax.cs
@@ -9,9 +9,8 @@ namespace Pzpn.Team.Web
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
